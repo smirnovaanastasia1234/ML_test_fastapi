58a22d94b32e26d82bdd53a44ae44e65c86c203e
smirnovaanastasia1234 <118005006+smirnovaanastasia1234@users.noreply.github.com>
2022-12-17 14:19:26 +0300

    add

--- test_main.py
+++ test_main.py
@@ -4,7 +4,21 @@ from main import app
 client = TestClient(app)
 
 
-def test_read_main():
-    response = client.get("/")
+def test_read_predict_positive():
+    response = client.post("/predict/",
+        json={"text": "I like machine learning!"}
+    )
+    json_data = response.json() 
+
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+    assert json_data['label'] == 'POSITIVE'
+
+
+def test_read_predict_negative():
+    response = client.post("/predict/",
+        json={"text": "I hate machine learning!"}
+    )
+    json_data = response.json() 
+
+    assert response.status_code == 200
+    assert json_data['label'] == 'NEGATIVE'
