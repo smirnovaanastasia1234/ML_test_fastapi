2adbb01ed60498901c019dee1f3dc56bf6b12ebc
EldarSB <118010872+EldarSB@users.noreply.github.com>
2022-12-22 20:11:54 +0300

    Update hw5.py

--- hw5.py
+++ hw5.py
@@ -11,29 +11,8 @@ classifier = pipeline("sentiment-analysis",
 
 result = st.button('Распознать текст')
 
-def test_predict_positive():
-    response = client.post("/predict/",
-        json={"text": "I like machine learning!"}
-    )
-    json_data = response.json() 
+def classif(txt):
+  return classifier(txt)[0]["label"]
 
-    assert json_data['label'] == 'POSITIVE'
-
-
-def test_predict_negative():
-    response = client.post("/predict/",
-        json={"text": "I hate machine learning!"}
-    )
-    json_data = response.json() 
-
-    assert json_data['label'] == 'NEGATIVE' 
-    
- def test_predict_neutral():
-    response = client.post("/predict/",
-        json={"text": "Sun"}
-    )
-    json_data = response.json() 
-
-    assert json_data['label'] == 'NEUTRAL' 
 st.write ("Тональность текста:")
-st.write(classifier(text)[0]["label"])
+st.write(classif(text))
